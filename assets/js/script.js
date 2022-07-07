683a2826fdb7b91032b3a778b609d7bf2e156458
Anita Jose <anitajose119@gmail.com>
2022-07-07 13:57:01 -0400

    color-code rows

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
+// add current date
 var DateTime = moment().format("dddd, MMMM Do");
+
 // show current date
 $("#currentDay").text(DateTime);
 
@@ -15,21 +17,41 @@ var timeArr = [
   "5 pm",
 ];
 
+startTime = 9;
 userInput = "";
 
 // loop through time range array, creating rows, hours, input fields, & save buttons
 for (i = 0; i < timeArr.length; i++) {
+  // create row to hold hour, input field, save button
   var eachRow = $("<div>").addClass("row");
   $(".container").append(eachRow);
+  // create hour
   var eachHour = $("<textarea>")
     .text(timeArr[i])
     .addClass("hour col-md-1 col-sm-1");
+
+  // create user input field, add classes, placeholder attr, & data-value attr
   var userInput = $("<input>")
     .attr("placeholder", "Add your notes here..")
+    .attr("data-value", i)
     .addClass("time-block col-md-10 col-sm-10");
+
+  // compare startTime with present time to color-code input field
+  if (startTime < moment().hour()) {
+    userInput.addClass("past");
+  } else if (startTime > moment().hour()) {
+    userInput.addClass("future");
+  } else {
+    userInput.addClass("present");
+  }
+  startTime++;
+
+  //   create save button
   var textLock = $("<button>")
-    .text("save")
+    .text("Save")
     .addClass("saveBtn col-md-1 col-sm-1");
+
+  // append time, input field, & save button to row
   eachRow.append(eachHour);
   eachRow.append(userInput);
   eachRow.append(textLock);
